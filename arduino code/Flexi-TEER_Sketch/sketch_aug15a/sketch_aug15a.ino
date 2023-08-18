884946816ee5ae1d3d11a9869bc5ae9476fb8578
Michael Anderson <md.anderson03@gmail.com>
2023-08-18 15:24:46 -0400

    testing on DAC and MUX
    Finished voltage testing on MUX chips and DAC

--- arduino code/Flexi-TEER_Sketch/sketch_aug15a/sketch_aug15a.ino
+++ arduino code/Flexi-TEER_Sketch/sketch_aug15a/sketch_aug15a.ino
@@ -1,6 +1,6 @@
 
 
-# define mux_enb_pin  11
+#define mux_enb_pin  11
 #define lvl_trans_en_pin 13
 #define a_pin A2
 #define b_pin A1
@@ -9,7 +9,8 @@
 
 
 
-
+int i = 1;
+int value = 0;
 
 
 void setup() {
@@ -18,28 +19,22 @@ void setup() {
 pinMode(batt_stby, OUTPUT);
 pinMode(mux_enb_pin, OUTPUT);
 pinMode(lvl_trans_en_pin, OUTPUT);
-pinMode(a_pin, OUTPUT);
+pinMode(a_pin, INPUT);
 pinMode(b_pin, OUTPUT);
 pinMode(c_pin, OUTPUT);
 pinMode(DAC0, OUTPUT);
 
 digitalWrite(lvl_trans_en_pin, HIGH);
-//digitalWrite(mux_enb_pin), HIGH);
+digitalWrite(mux_enb_pin, HIGH);
 
 
-digitalWrite(a_pin, HIGH);
-digitalWrite(b_pin, HIGH);
-digitalWrite(c_pin, HIGH);
+//digitalWrite(a_pin, LOW);
+digitalWrite(b_pin, LOW);
+digitalWrite(c_pin, LOW);
 digitalWrite(batt_stby, LOW);
 
-analogWrite(DAC0, 384);
 
-
-
-int level = 500;
-analogReference(500);
-
-
+Serial.begin(9600);
 
 
 }
@@ -50,10 +45,20 @@ void loop() {
 
 
 
+analogWrite(DAC0, i);
+delay(1000);
+value = analogRead(a_pin);
+Serial.print(value);
+Serial.print('_');
+Serial.print(i);
+Serial.println(' ');
+delay(9000);
 
-//analogWrite(DAC0, 384);
-//delay(1000);
+i = i +100;
 
+if(i > 1002){
+  i = 1;
+}
 
 
 }
---
+++ arduino code/testing code/DAC_testing_code/DAC_testing_code.ino
@@ -0,0 +1,65 @@
+
+
+#define mux_enb_pin  11
+#define lvl_trans_en_pin 13
+#define a_pin A2
+#define b_pin A1
+#define c_pin A3
+#define batt_stby 9
+
+
+
+int i = 1;
+int value = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(batt_stby, OUTPUT);
+pinMode(mux_enb_pin, OUTPUT);
+pinMode(lvl_trans_en_pin, OUTPUT);
+pinMode(a_pin, INPUT);
+pinMode(b_pin, OUTPUT);
+pinMode(c_pin, OUTPUT);
+pinMode(DAC0, OUTPUT);
+
+digitalWrite(lvl_trans_en_pin, HIGH);
+digitalWrite(mux_enb_pin, HIGH);
+
+
+//digitalWrite(a_pin, LOW);
+digitalWrite(b_pin, LOW);
+digitalWrite(c_pin, LOW);
+digitalWrite(batt_stby, LOW);
+
+analogWriteResolution(10);
+
+Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+analogWrite(DAC0, i);
+delay(400);
+value = analogRead(a_pin);
+Serial.print(value);
+Serial.print("_");
+Serial.print(i);
+Serial.println(' ');
+delay(2000);
+
+i = i + 50;
+
+if(i>1000){
+  i = 1;
+}
+
+
+
+
+}
